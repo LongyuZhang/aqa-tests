2a9b536b615e76ca7f9af2360289a1dd7570c2b0
Longyu Zhang <longyu.zhang@ibm.com>
2022-03-17 14:21:48 -0400

    Support criu docker test - Add suport for criu docker test
    Signed-off-by: Longyu Zhang <longyu.zhang@ibm.com>

--- external/functional-test/test.sh
+++ external/functional-test/test.sh
@@ -20,14 +20,31 @@ echo "TEST_JDK_HOME is : $TEST_JDK_HOME"
 export BUILD_LIST=functional
 
 echo "testtest set up criu environment"
-apt install pkg-config nftables iproute2 libcap-dev libnl-3-dev libnet1-dev libnet-cpp-dev libnetxx-dev libaio-dev asciidoc xmlto libbsd-dev libnftables-dev protobuf-c-compiler libprotobuf-dev libprotobuf-c1 libprotobuf-c-dev protobuf-compiler python-protobuf
-git clone https://github.com/xemul/criu.git criu
-cd criu && make clean && make && make install
-setcap cap_sys_time,cap_dac_override,cap_chown,cap_setpcap,cap_setgid,cap_audit_control,cap_dac_read_search,cap_net_admin,cap_sys_chroot,cap_sys_ptrace,cap_fowner,cap_kill,cap_fsetid,cap_sys_resource,cap_setuid,cap_sys_admin=eip /usr/local/sbin/criu: \
+cd /tmp
+criu_v=3.16.1
+wget https://github.com/checkpoint-restore/criu/archive/refs/tags/v${criu_v}.tar.gz
+ls
+
+tar -xzf v${criu_v}.tar.gz
+ls
+cd criu-${criu_v}
+make
+make install
+
+
+criu -V
+
 cd ..
+rm -rf v${criu_v}.tar.gz criu-${criu_v}
+setcap cap_chown,cap_dac_override,cap_dac_read_search,cap_fowner,cap_fsetid,cap_kill,cap_setgid,cap_setuid,cap_setpcap,cap_net_admin,cap_sys_chroot,cap_sys_ptrace,cap_sys_admin,cap_sys_resource,cap_sys_time,cap_audit_control=eip /usr/local/sbin/criu
+# apt install pkg-config nftables iproute2 libcap-dev libnl-3-dev libnet1-dev libnet-cpp-dev libnetxx-dev libaio-dev asciidoc xmlto libbsd-dev libnftables-dev protobuf-c-compiler libprotobuf-dev libprotobuf-c1 libprotobuf-c-dev protobuf-compiler python-protobuf
+# git clone https://github.com/xemul/criu.git criu
+# cd criu && make clean && make && make install
+# setcap cap_sys_time,cap_dac_override,cap_chown,cap_setpcap,cap_setgid,cap_audit_control,cap_dac_read_search,cap_net_admin,cap_sys_chroot,cap_sys_ptrace,cap_fowner,cap_kill,cap_fsetid,cap_sys_resource,cap_setuid,cap_sys_admin=eip /usr/local/sbin/criu: \
+# cd ..
 
 cd /aqa-tests
-./get.sh --openj9_repo https://github.com/longyuzhang/openj9.git --openj9_branch criu1
+./get.sh
 
 cd /aqa-tests/TKG
 
